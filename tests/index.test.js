eee5b4c1901a6883a9308d861599f2269ee8f319
Gagandeep Singh <gagan.ddun@gmail.com>
2019-12-21 22:34:58 +1100

    added products info / updated test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,13 @@
+const { PRODUCTS } = require('../utils/util');
+
 describe("Main tests", () => {
-    describe("Check the products price", () => {
-        test("Should get correct products price", () => {
-            expect(1).toBe(2);
+    describe("Check the products info", () => {
+        console.log(PRODUCTS);
+        const { super_ipad, macbook_pro, apple_tv, vga_adaptor } = PRODUCTS;
+        test("Check ipad product info", () => {
+            expect(super_ipad.name).toBe(549.99);
+            expect(super_ipad.sku).toBe(1399.99);
+            expect(super_ipad.price).toBe(109.50);
         })
     })
 });
---
+++ utils/util.js
@@ -0,0 +1,24 @@
+const PRODUCTS = {
+    super_ipad: {
+        name: 'Super iPad',
+        sku: 'ipd',
+        price: 549.99
+    },
+    macbook_pro: {
+        name: 'Macbook Pro',
+        sku: 'mbp',
+        price: 1399.99
+    },
+    apple_tv: {
+        name: 'Apple TV',
+        sku: 'atv',
+        price: 109.50
+    },
+    vga_adaptor: {
+        name: 'VGA Adapter',
+        sku: 'vga',
+        price: 30.00
+    }
+}
+
+exports.PRODUCTS = PRODUCTS;
