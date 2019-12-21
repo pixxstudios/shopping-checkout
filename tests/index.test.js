3c38470d3a51894a07a15ce7dd24177ad29f538b
Gagandeep Singh <gagan.ddun@gmail.com>
2019-12-21 22:19:38 +1100

    initial setup / tests

---
+++ tests/index.test.js
@@ -0,0 +1,7 @@
+describe("Main tests", () => {
+    describe("Check the products price", () => {
+        test("Should get correct products price", () => {
+            expect(1).toBe(2);
+        })
+    })
+});
