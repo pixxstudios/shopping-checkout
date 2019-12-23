848437a5f742758a1e0d62f0a74a7067d514af76
Gagandeep Singh <gagan.ddun@gmail.com>
2019-12-23 21:26:55 +1100

    fixed atv price

--- utils/price-rules.js
+++ utils/price-rules.js
@@ -17,7 +17,6 @@ const getTotalItems = (Products) => {
 }
 
 const price_rules = (Products) => {
-    console.log(getTotalItems(Products));
     const Items = getTotalItems(Products);
     let total_bill = 0;
 
@@ -25,17 +24,14 @@ const price_rules = (Products) => {
         switch(item) {
             case 'atv': {
                 const total_apple_tv = Items[item];
-                console.log('total_apple_tv ', total_apple_tv);
                 if(total_apple_tv < 3) {
                     total_bill = total_apple_tv * PRODUCTS.apple_tv.price;
                 } else {
-                    total_bill = (total_apple_tv) * PRODUCTS.apple_tv.price
-                    - (total_apple_tv / 3) * PRODUCTS.apple_tv.price;
+                    total_bill = (total_apple_tv - Math.floor(total_apple_tv/3)) * PRODUCTS.apple_tv.price;
                 }
             }
         }
     }
-    console.log('total_bill ', total_bill);
     return total_bill;
 };
 
